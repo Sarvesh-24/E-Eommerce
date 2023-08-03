e07a22ac7fdf5e5bbdbc2c640f06a4cd0664b3cb
Sarvesh <sarvesh.somaiya.edu>
2023-08-03 16:54:21 +0530

    done with frontend setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,12 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM  from 'react-dom'
-
+import {BrowserRouter} from 'react-router-dom'
 import { App } from './App'
 
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<ChakraProvider>
+   <BrowserRouter>
+        <App />
+   </BrowserRouter>
+</ChakraProvider>, document.querySelector('#root'))
