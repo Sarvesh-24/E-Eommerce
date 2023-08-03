c3937f8572b533d7e1a664f5d2409bac82ce5408
Sarvesh <sarvesh.somaiya.edu>
2023-08-03 18:58:48 +0530

    setup backend

---
+++ backend/data/products.js
@@ -0,0 +1,117 @@
+const products = [
+	{
+		_id: 1 ,
+		name: 'Red chair',
+		img: '/images/img_red',
+		description:
+			'Product Dimensions : 103.0 cm x 72.0 cm x 76.0cm Country of Origin : INDIA',
+		brand: 'Lebowski',
+		category: 'Recliners',
+		price: 48000,
+		countInStock: 6,
+		rating: 4.5,
+		numReviews: 4,
+	},
+	{
+		_id: 2 ,
+		name: 'Desmond Floor Lamp',
+		img: '/images/Desmond_Floor_Lamp_2.jpg',
+
+		description:
+			'Product Dimensions : 159.0 cm x 53.0 cm x 53.0cm Country of Origin : INDIA',
+		brand: 'Desmond ',
+		category: 'Floor Lamp',
+		price: 8500,
+		countInStock: 10,
+		rating: 4.4,
+		numReviews: 4,
+	},
+	{
+		_id: 3 ,
+		name: 'Pashe Fabric Accent Chair In Teal Colour',
+		img: '/images/Pashe_Dining_Chair_TL.jpg',
+		
+		description:
+			'Product Dimensions : 81.0 cm x 48.0 cm x 55.0cm Country of Origin : CHINA',
+		brand: 'Pashe Fabric',
+		category: 'Chair',
+		price: 6000,
+		countInStock: 18,
+		rating: 4.5,
+		numReviews: 3.8,
+	},
+	{
+		_id: 4 ,
+		name: 'Madden',
+		img: '/images/Madden_Shoe_Cabinet_Mahogany_3.jpg',
+		
+		description:
+			'Product Dimensions : 93.0 cm x 71.0 cm x 38.0cm Country of Origin : INDIA',
+		brand: 'Madden',
+		category: 'Shoe Cabinet',
+		price: 12000,
+		countInStock: 8,
+		rating: 4.8,
+		numReviews: 12,
+	},
+	{
+		_id: 5,
+		name: 'Janet 2 Fabric Loveseat ',
+		img: '/images/Janet_Lapis_Blue_2.jpg',
+		description:
+			'Product Dimensions : 68.0 cm x 142.0 cm x 76.0cm Country of Origin : INDIA',
+		brand: 'Janet',
+		category: 'Loveseat',
+		price: 43000,
+		countInStock: 6,
+		rating: 4.9,
+		numReviews: 8,
+	},
+	{
+		_id: 6,
+		name: 'Epsilon Rectangular Solid Wood',
+		img: '/images/Epsilon_Coffe_Table_MH_03.jpg',
+		
+		description:
+			'Product Dimensions : 36.0 cm x 96.0 cm x 55.0cm Country of Origin : INDIA',
+		brand: 'Epsilon',
+		category: 'coffe Table',
+		price: 8000,
+		countInStock: 21,
+		rating: 4.1,
+		numReviews: 3,
+	},
+	{
+		_id: 7,
+		name: 'Speckle Table Lamp Round',
+		img: '/images/Speckle_Table_Lamp_Round_2.jpg',
+		
+		description:
+			'Product Dimensions : 41.0 cm x 15.0 cm x 15.0cm Country of Origin : INDIA',
+		brand: 'Speckle',
+		category: 'Table Lamp',
+		price: 7500,
+		countInStock: 25,
+		rating: 4.4,
+		numReviews: 3,
+	},
+	{
+		_id: 8,
+		name: 'Kaya Table Lamp',
+		img: '/images/Kaya_Table_Lamp_1.jpg',
+		
+		description:
+			'Product Dimensions : 43.0 cm x 28.0 cm x 28.0cm Country of Origin : INDIA',
+		brand: 'Chanel',
+		category: 'Table Lamp',
+		price: 9500,
+		countInStock: 0,
+		rating: 4.2,
+		numReviews: 5,
+	},
+];
+
+
+module.exports = products;
+
+
