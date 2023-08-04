2500c7f9aec97570d11c8b20b158277ab306bc4e
Sarvesh <sarvesh.somaiya.edu>
2023-08-04 09:42:53 +0530

    created database models

---
+++ backend/models/userModel.js
@@ -0,0 +1,42 @@
+
+import mongoose, { Mongoose } from "mongoose";
+
+
+
+
+const userSchema = mongoose.Schema({
+
+    name:{
+
+        type: String,
+        required: true
+
+    },
+
+    email:{
+
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password:{
+
+        type: String,
+        required: true
+    },
+    isAdmin:{
+
+        type: Boolean,
+        required: true,
+        default: false
+
+    },
+
+
+}, {timestamps: true})
+
+
+export const User = mongoose.model('User' , userSchema);
+
+
