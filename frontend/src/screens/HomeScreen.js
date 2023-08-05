d25a650e48e0c6dac3d716c8823968e7e3c93eb3
Sarvesh <sarvesh.somaiya.edu>
2023-08-05 10:19:11 +0530

    added Loader and Message components to the Home screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,36 +1,40 @@
 import { Grid, Heading } from "@chakra-ui/react";
 import { useEffect } from "react";
-import {useDispatch, useSelector} from 'react-redux'
-
+import { useDispatch, useSelector } from "react-redux";
 
 import { Product } from "../component/Product";
-import { listProducts } from '../actions/productActions'
-
+import { listProducts } from "../actions/productActions";
+import { Message } from "../component/Message";
+import { Loader } from "../component/Loader";
 
 export const HomeScreen = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch(); 
+  const productList = useSelector((state) => state.productList);
 
-    const productList = useSelector(state => state.productList);
+  const { loading, error, products } = productList;
 
-    const {loading,error,products} = productList;
-
-    useEffect(() => {
-
-      dispatch(listProducts());
-
-    },[])
+  useEffect(() => {
+    dispatch(listProducts());
+  }, []);
 
   return (
     <>
       <Heading as="h2" mb="8" fontSize="3xl">
         Latest Product
+      </Heading>
+
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message type="error">{error}</Message>
+      ) : (
         <Grid templateColumns="repeat(4, 1fr)" gap="8">
           {products.map((product) => (
             <Product key={product._id} product={product} />
           ))}
         </Grid>
-      </Heading>
+      )}
     </>
   );
 };
