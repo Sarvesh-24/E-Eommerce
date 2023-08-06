3c6ad4566816735c45019b6e6553e31e37fd49a5
Sarvesh <sarvesh.w@somaiya.edu>
2023-08-06 15:54:20 +0530

    created user reducer, userActions and user constants

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,25 +1,42 @@
+import {
+  legacy_createStore as CreateStore,
+  combineReducers,
+  applyMiddleware,
+} from "redux";
 
-import {legacy_createStore as CreateStore, combineReducers, applyMiddleware} from 'redux'
-
-import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import {productListReducer,productDetailReducer} from './reducers/productReducer'
-import { cartReducer } from './reducers/cartReducer';
-
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  productListReducer,
+  productDetailReducer,
+} from "./reducers/productReducer";
+import { cartReducer } from "./reducers/cartReducer";
+import { userReducer } from "./reducers/userReducer";
 
 const reducer = combineReducers({
-    productList:productListReducer,
-    productDetails:productDetailReducer,
-    cart:cartReducer,
+  productList: productListReducer,
+  productDetails: productDetailReducer,
+  cart: cartReducer,
+  userLogin: userReducer,
 });
 
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')): [];
 
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+? JSON.parse(localStorage.getItem("cartItems"))
+: [];
 
-const initialState = {cart: {cartItems: cartItemsFromStorage}};
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : {};
+
+const initialState = { cart: { cartItems: cartItemsFromStorage }, userLogin:{userInfo:userInfoFromStorage} };
 
 const middlewares = [thunk];
 
-const store = CreateStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middlewares)));
+const store = CreateStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
 
 export default store;
